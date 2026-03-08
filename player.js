cef8ebc20e511cda1988465b573c48e52fb2527c
shqawthmw060-sketch <shqawthmw060@gmail.com>
2026-03-08 23:51:13 +0200

    Update player.js

--- player.js
+++ player.js
@@ -27,3 +27,34 @@ function saveHistory(url){
 localStorage.setItem("lastChannel",url)
 
 }
+let video = document.getElementById("video")
+
+function playStream(url){
+
+if(Hls.isSupported()){
+
+let hls = new Hls()
+
+hls.loadSource(url)
+
+hls.attachMedia(video)
+
+}else{
+
+video.src = url
+
+}
+
+saveHistory(url)
+
+}
+
+function saveHistory(url){
+
+let history = JSON.parse(localStorage.getItem("history") || "[]")
+
+history.unshift(url)
+
+localStorage.setItem("history",JSON.stringify(history.slice(0,10)))
+
+}
